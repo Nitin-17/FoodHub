0cc180bf4743d9430617c919a416b8ea2d21e90f
Nitin Saini <49126948+Nitin-17@users.noreply.github.com>
2020-10-18 12:13:35 +0530

    Add files via upload

---
+++ New folder/proto1.js
@@ -0,0 +1,69 @@
+
+		var a=0,b=0,c=0,d=0,e=0;
+		function add() {
+			a++;
+			document.getElementById('value').innerHTML=a;
+		   	angular.module('myapp',[])
+		.controller('todoController', function($scope){
+			$scope.tasks=[];
+			$scope.add=function(){
+				$scope.tasks.push($scope.title);
+			}
+			$scope.delete=function(){
+				$scope.tasks.splice(this.$index,1);
+			}
+		})
+
+		}
+		function remove(){
+			if (a>0) {
+            a--;
+            document.getElementById('value').innerHTML=a;
+        )
+			}
+		}
+		function add1() {
+			b++;
+			document.getElementById('value1').innerHTML=b;
+
+		}
+		function remove1(){
+			if (b>0) {
+            b--;
+            document.getElementById('value1').innerHTML=b;
+			}
+		}
+		function add2() {
+			c++;
+			document.getElementById('value2').innerHTML=c;
+
+		}
+		function remove2(){
+			if (c>0) {
+            c--;
+            document.getElementById('value2').innerHTML=c;
+			}
+		}
+		function add3() {
+			d++;
+			document.getElementById('value3').innerHTML=d;
+
+		}
+		function remove3(){
+			if (d>0) {
+            d--;
+            document.getElementById('value3').innerHTML=d;
+			}
+		}
+		function add4() {
+			e++;
+			document.getElementById('value4').innerHTML=e;
+
+		}
+		function remove4(){
+			if (e>0) {
+            e--;
+            document.getElementById('value4').innerHTML=e;
+			}
+		}
+	    
